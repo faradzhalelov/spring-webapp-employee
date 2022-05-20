53e50447c9329bd1ace1800c4d2eb4679a59c3ff
faradzhalelov <faradzhalelov@gmail.com>
2022-05-20 16:31:05 +0400

    Spring Boot Thymeleaf CRUD Database Project

---
+++ src/main/java/com/example/springbootthymeleafwebapp/SpringbootThymeleafWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootthymeleafwebapp/SpringbootThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
